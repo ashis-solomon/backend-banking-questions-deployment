a35c4d62d4c3b541e2809a380fb2428f74cc8612
ashis-solomon <ashissolomon24@gmail.com>
2023-06-16 19:00:28 +0530

    update

--- core/core/settings.py
+++ core/core/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 # ALLOWED_HOSTS = ["*"]
 # CORS_ORIGIN_ALLOW_ALL = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.onrender.com', '.vercel.app']
 
 # Application definition
 
@@ -138,6 +138,7 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # User Defined
 
